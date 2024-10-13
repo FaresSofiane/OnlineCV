aa1129a58e1ef340147de54a86164c3166fff053
Sofiane Fares <sofiane.fares77@outlook.fr>
2024-10-13 19:35:38 +0200

    ajout users/ et cv/
    création des endpoints pour ajouter/supprimer un cv et pour voir les cv en public de tout les users

---
+++ backend/models/CV_Exp_Pedago.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const CVExpPedagoSchema = new mongoose.Schema({
+    cv_id: { type: mongoose.Schema.Types.ObjectId, ref: 'CV', required: true },
+    nom_diplome: { type: String, required: true },
+    certificateur: { type: String, required: true },
+    date: { type: Date, required: true },
+    commentaire: { type: String }
+});
+
+module.exports = mongoose.model('CV_Exp_Pedago', CVExpPedagoSchema);
---
+++ backend/routes/user.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../models/User');
+const CV = require('../models/CV');
+const authMiddleware = require('../middleware/authMiddleware');
+
+
+router.get('/users/cvs/public', authMiddleware, async (req, res) => {
+    try {
+        const users = await User.find();
+
+        const usersWithPublicCVs = await Promise.all(users.map(async user => {
+            const publicCVs = await CV.find({ user_id: user._id, private: false });
+
+            return {
+                username: user.username,
+                email: user.email,
+                cvs: publicCVs
+            };
+        }));
+
+        res.status(200).json(usersWithPublicCVs);
+    } catch (error) {
+        res.status(500).json({ error: 'Erreur lors de la récupération des utilisateurs et de leurs CVs publics' });
+    }
+});
+
+module.exports = router;
