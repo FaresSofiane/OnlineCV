ab82fa7fe92bf548b301db11a8da06579bbfe534
Sofiane Fares <sofiane.fares77@outlook.fr>
2024-10-20 22:01:55 +0200

    netlify push

---
+++ backend/app.js
@@ -0,0 +1,24 @@
+// src/app.js
+
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+require('dotenv').config();
+
+const authMiddleware = require('./middleware/authMiddleware');
+const apiRouter = require('./routes');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Connexion à la base de données
+mongoose
+    .connect(process.env.DATABASE_URL)
+    .then(() => console.log('Database connected'))
+    .catch((error) => console.log(`Database connection error: ${error.message}`));
+
+// Routes
+app.use('/api', apiRouter);
+
+module.exports = app;
