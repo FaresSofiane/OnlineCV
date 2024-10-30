cf5a8ec4e779d92a743fa974c04f1e5deeb5f751
Sofiane Fares <sofiane.fares77@outlook.fr>
2024-10-30 14:36:08 +0100

    Update user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
-const CV = require('../models/CV');
+const CV = require('../models/CV.js');
 const authMiddleware = require('../middleware/authMiddleware');
 
 
